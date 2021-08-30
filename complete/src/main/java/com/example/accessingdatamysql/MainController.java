e746f399b8e8c9da7d765d3368abb1611c09072a
wuhepeng <1845664903@qq.com>
2021-08-30 16:56:15 +0800

    增加条件查询

--- complete/src/main/java/com/example/accessingdatamysql/MainController.java
+++ complete/src/main/java/com/example/accessingdatamysql/MainController.java
@@ -2,11 +2,12 @@ package com.example.accessingdatamysql;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
 @Controller	// This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
@@ -28,9 +29,44 @@ public class MainController {
 		return "Saved";
 	}
 
-	@GetMapping(path="/all")
+	@PostMapping(path="/update") // Map ONLY POST Requests
+	public @ResponseBody String addNewUser (@RequestParam Integer id, @RequestParam String name
+			, @RequestParam String email) {
+		// @ResponseBody means the returned String is the response, not a view name
+		// @RequestParam means it is a parameter from the GET or POST request
+
+		User n = new User();
+		n.setId(id);
+		n.setName(name);
+		n.setEmail(email);
+		userRepository.save(n);
+		return "Saved";
+	}
+
+	@PostMapping(path="/all")
 	public @ResponseBody Iterable<User> getAllUsers() {
 		// This returns a JSON or XML with the users
 		return userRepository.findAll();
 	}
+
+	@PostMapping(path="/condition")
+	public @ResponseBody
+	List<User> getUsers(@RequestParam String param) {
+		// This returns a JSON or XML with the users
+		List<User> list = new ArrayList<>();
+		 for (User user : userRepository.findAll()){
+		 	if(user.getEmail().toLowerCase().contains(param)){
+		 		list.add(user);
+			}
+		 }
+		 return list;
+	}
+
+	@GetMapping(path = "id")
+	public @ResponseBody
+	Optional<User> getUserById(@RequestParam Integer id){
+		return userRepository.findById(id);
+	}
+
+
 }
